df98bdf582e13aeb5f1f7798829d0e453fd32f94
Suryakant <ssuryakanta696@gmail.com>
2023-11-01 16:41:43 +0530

    file name change

---
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+
+    def initate_data_ingestion(self):
+        pass
+
---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+class DataTransformer:
+    def __init__(self):
+        pass
+
+    def get_data_transformer(self):
+        pass
+
